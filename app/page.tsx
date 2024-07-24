391d1b9bdda715cddf71596b0b4e7daf1e5322e3
shayan.heidary <shayan.heidary@knime.com>
2024-07-24 17:35:16 +0200

    update caching on reload

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export const maxDuration = 60;
 
+import { revalidatePath } from "next/cache";
 import Main from "./components/Main";
 import React from "react";
 export default function Home() {
+  revalidatePath("/");
+
 
   return (
     <div className="w-full  rounded-xl h-screen mx-auto">
