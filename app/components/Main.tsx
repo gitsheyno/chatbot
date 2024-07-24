b67dbaf2331f28e85d733f9a61b98a681151fc21
shayan.heidary <shayan.heidary@knime.com>
2024-07-24 17:55:14 +0200

    move revalidate to main component

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -2,9 +2,12 @@ import Content from "./Content";
 import { fetchHistory } from "../seed/seedDB";
 import React from "react";
 
+import { revalidatePath } from "next/cache";
 const data = await fetchHistory();
 
 export default function Main() {
+
+  revalidatePath("/")
   return (
     <div className=" h-full flex">
         <Content ctx={data} />
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 export const maxDuration = 60;
 
-import { revalidatePath } from "next/cache";
 import Main from "./components/Main";
 import React from "react";
 export default function Home() {
-  revalidatePath("/");
 
 
   return (
