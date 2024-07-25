6bbd943c5ab364017755774f9608d9569ad6d247
shayan.heidary <shayan.heidary@knime.com>
2024-07-25 09:49:57 +0200

    fixed dynamic revalidation

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -5,6 +5,8 @@ import React from "react";
 import { revalidatePath } from "next/cache";
 const data = await fetchHistory();
 
+export const dynamic = 'force-dynamic';
+
 export default function Main() {
 
   revalidatePath("/")
